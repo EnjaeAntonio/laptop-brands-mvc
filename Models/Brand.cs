77e444cc4a056fdc0774589cdedea812d9ea385d
Enjae Antonio <Enjaeantonio@outlook.com>
2023-02-13 14:09:30 -0600

    controller created and views auto rendered by Visual Studio. Fixing bugs

---
+++ Models/Brand.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCAssessment.Data;
+
+namespace MVCAssessment.Models
+{
+    public class Brand
+    {
+        private readonly int _brandId;
+        public int BrandId { get { return _brandId; } }
+        private string _name;
+        public string Name { get { return _name; } }
+        public HashSet<Laptop> Laptops { get; set; }
+
+        public Brand(string name)
+        {
+            _brandId = Context.GetIdCount();
+            _name = name;
+            Laptops = new HashSet<Laptop>();
+        }
+    }
+}
---
+++ Models/ViewModels/CompareLaptopsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVCAssessment.Models.ViewModels
+{
+    public class CompareLaptopsViewModel
+    {
+        public IEnumerable<SelectListItem> LaptopLists { get; set; }
+        public Laptop laptop1 { get; set; }
+        public Laptop laptop2 { get; set;  }
+    }
+}
